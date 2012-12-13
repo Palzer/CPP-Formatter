fd464459b26eaef960f7a9fab9be16a2d69725b9
David Palzer <davidpalzer@gmail.com>
2012-12-13 02:12:02 -0800

    changed how the newline is built. Now is built up as the chars are read from the old line. this facilitates putting braces on their own line. next is putting newlines after ; ony if they arent already the end

--- format.cpp
+++ format.cpp
@@ -3,6 +3,7 @@
 #include <string>
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
 using namespace std;
 
 void process(char* filename);
@@ -14,14 +15,14 @@ int main(int argc, char *argv[])
 	for (int i = 1; i < argc; i ++)
 	{
 		fprintf(stderr,"Formatting file: %s\n",argv[i]);
-		if (validfile(argv[i]))
-		{
+		//if (validfile(argv[i]))
+		//{
 			process(argv[i]);
-		}
-		else
-		{
-			fprintf(stderr,"	Error: File is not .cpp\n");
-		}
+		//}
+		//else
+		//{
+		//	fprintf(stderr,"	Error: File is not .cpp\n");
+		//}
 	}
 	return 0;
 }
@@ -38,8 +39,9 @@ void process(char* filename)
 	string line = "";
 	bool quote = false;
 	bool tick = false;
-	bool finline = false;
-	bool binline = false;
+	char newchar[2];
+	newchar[0] = ' ';
+	newchar[1] = 0;
 	
 	strcpy(newfilename,filename);
 	strcat(newfilename,"new");
@@ -53,39 +55,69 @@ void process(char* filename)
 	while (myfile.good())
 	{
 		newline = "";
-		finline = false;
-		binline = false;
 		getline(myfile,line);
 		//cout << line;
 		while(line[0] == 9 || line[0] == 32){
 			line.erase(0,1);
 		}
+		cout << "current line is :" << line << endl;
+		if (line[0] != '{' && line[0] != '}') newline.append(numtab,9);
 		for (int i = 0; i < line.length(); i++)
 		{
 			if ((line[i] == '{') and !quote)
 				{
-					finline = true;
+					if (i != 0)
+					{
+						 newline.append("\n");
+						newline.append(numtab,9);
+					}
+					newline.append("{");
+					
 					numtab++;
+					fprintf(stderr,"opening: changing tabs to %i\n",numtab);
+					if (i != line.length() - 1)
+					{
+						newline.append("\n");
+						newline.append(numtab,9);
+					}
 				}
 				else if(line[i] == '}' and !quote)
-			{
-				if (i != 0) binline = true;
-				if (numtab > 0)
 				{
-					numtab--;		
-				}				
-			}
+					if (numtab > 0)
+					{		
+						numtab--;
+						fprintf(stderr,"closing: changing tabs to %i\n",numtab);		
+					}
+					if (i != 0)
+					{
+						newline.append("\n");
+						newline.append(numtab,9);
+					}
+					newline.append("}");	
+					if (i != line.length()-1)
+					{
+						newline.append("\n");		
+						newline.append(numtab,9);
+					}	
+				}
 			else if((line[i] == '"') and !tick)
 			{
 				quote = !quote;
+				newline.append("\"");
 			}
 			else if((line[i] == 39/*tick*/) and !quote)
 			{
 				tick = !tick;
+				newline.append("'");
+			}
+			else
+			{
+				newchar[0] = line[i];
+				newline.append(newchar);
 			}
 		}
 		
-		if (numtab < 0) numtab = 0;
+		/*if (numtab < 0) numtab = 0;
 		if ((finline) and (!binline) and (numtab > 0))
 		{
 			newline.append(numtab-1,9);
@@ -101,7 +133,7 @@ void process(char* filename)
 			newline.append(numtab,9);
 			//cout << numtab << " indents on line '" << line << "'" << endl;
 		}
-		newline.append(line);
+		newline.append(line);*/
 		if(myfile.good())
 		{
 			newline.append("\n");
